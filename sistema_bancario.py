7572aafed2c0abb67b3dcae258a5366cbd6da427
AllexandreVitor <allexandrevitor.av@gmail.com>
2025-05-05 10:56:07 -0300

    feat: add filtrar_cliente function

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -187,3 +187,8 @@ def menu():
     [q]\tSair
     => """
     return input(textwrap.dedent(menu))
+
+
+def filtrar_cliente(cpf, clientes):
+    clientes_filtrados = [cliente for cliente in clientes if cliente.cpf == cpf]
+    return clientes_filtrados[0] if clientes_filtrados else None
