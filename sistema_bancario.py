ad21d2d63ee33e1cf2e68f709b576395a15415cc
AllexandreVitor <allexandrevitor.av@gmail.com>
2025-05-05 10:38:18 -0300

    feat: add Saque class

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -143,6 +143,21 @@ class Transacao(ABC):
         pass
 
 
+class Saque(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+
+    @property
+    def valor(self):
+        return self._valor
+    
+    def registrar(self, conta):
+        sucesso_transacao = conta.sacar(self.valor)
+
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)
+
+
 # Função para o menu
 def menu():
     menu ="""\n
