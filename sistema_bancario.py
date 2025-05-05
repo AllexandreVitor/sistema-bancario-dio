566682a0ad95a64df3ba819aa92b80837ee37917
AllexandreVitor <allexandrevitor.av@gmail.com>
2025-05-05 10:42:39 -0300

    feat: add Deposito class

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -158,6 +158,22 @@ class Saque(Transacao):
             conta.historico.adicionar_transacao(self)
 
 
+class Deposito(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+
+        @property
+        def valor(self):
+            return self._valor
+        
+        def registrar(self, conta):
+            sucesso_transacao = conta.depositar(self.valor)
+
+            if sucesso_transacao:
+                conta.historico.adicionar_transacao(self)
+
+
+
 # Função para o menu
 def menu():
     menu ="""\n
